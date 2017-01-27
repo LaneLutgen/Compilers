47da67d9839166564f7715ecec9bcee810c55a2d
Lane Lutgen <lutgenlane@gmail.com>
2017-01-27 14:19:58 -0700

    Added in the demo code for reference

--- src/Driver.java
+++ src/Driver.java
@@ -1,14 +1,41 @@
 
+import java.io.*;
 import org.antlr.runtime.*;
 
 public class Driver {
 
 	public static void main(String[] args) {
-		//Open micro file input
+		try{
+		//Open micro file input (replace System.in with actual file stream)
+		CharStream stream = new ANTLRInputStream(System.in);
 		
+		// Create a new lexer on the specified 'CharStream'
+	    //DemoLexer lexer = new DemoLexer(stream);
+
+	      // Lexer types are enumerated, so we need to create a
+	      // 'Vocabulary' to lookup the symbol names from the
+	      // enumerated value. Note: The 'EOF' token has a value
+	      // of -1, and all other reules are enumerated from 0.
+	      //Vocabulary vocab = lexer.getVocabulary();
+
+	      // A simple loop that prints out all token symbols and
+	      // their literal values. This is not quite set up as you
+	      // want for the grading script - you will need to change
+	      // the format a bit.
+	      //Token token = null;
+	      //do {
+	      //  token = lexer.nextToken();
+	      //  System.out.println("\t" + vocab.getSymbolicName(token.getType()) +
+	      //                     "\t\t" + token.getText());
+	      //} while (token.getType() != Token.EOF);
 		//Run antlr on program to generate tokens
 		
 		//Write tokens to an output micro file
+		}
+		catch(Exception e)
+		{
+			System.out.println("Bad things happened");
+		}
 	}
 
 }
