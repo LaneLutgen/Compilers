0170bab2be93309ffd33501568820898b70c053a
Lane Lutgen <lutgenlane@gmail.com>
2017-02-07 20:10:33 -0700

    Fixed Micro.sh

--- Micro.sh
+++ Micro.sh
@@ -6,11 +6,13 @@ for i in `ls ANTLR/*.jar`
   THE_CLASSPATH=${THE_CLASSPATH}:${i}
 done
 
+THE_CLASSPATH=${THE_CLASSPATH}:ANTLR/generated
+THE_CLASSPATH=${THE_CLASSPATH}:src
+
+echo ${THE_CLASSPATH}
+
 javac -classpath ".:${THE_CLASSPATH}" ANTLR/generated/*.java src/task/*.java src/object/*.java src/driver/*.java src/client/*.java
 
-if [ $? -eq 0 ]
-then
-  echo "compile worked!"
-fi
+java -cp ${THE_CLASSPATH} client.Main
 
-sleep 5
+sleep 2
