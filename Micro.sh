63b5654ebd1d426194c568e1b7c70dd90592d93e
LaneLutgen <lutgenlane@gmail.com>
2017-02-07 19:08:47 -0700

    Updated Micro.sh (should be working now)

--- Micro.sh
+++ Micro.sh
@@ -1,15 +1,12 @@
-#!/bin/sh
-which java
+#!/bin/bash
 
 THE_CLASSPATH=
-PROGRAM_NAME=Main.java
-cd src
-for i in `ls ../ANTLR/*.jar`
+for i in `ls ANTLR/*.jar`
   do
   THE_CLASSPATH=${THE_CLASSPATH}:${i}
 done
 
-javac -classpath ".:${THE_CLASSPATH}" $PROGRAM_NAME
+javac -classpath ".:${THE_CLASSPATH}" ANTLR/generated/*.java src/task/*.java src/object/*.java src/driver/*.java src/client/*.java
 
 if [ $? -eq 0 ]
 then
