487da333ba891336a979bc364f9d0940654b49b0
LaneLutgen <lutgenlane@gmail.com>
2017-02-07 20:46:06 -0700

    Forgot to add the argument for the input

--- Micro.sh
+++ Micro.sh
@@ -11,6 +11,6 @@ THE_CLASSPATH=${THE_CLASSPATH}:src
 
 javac -classpath ".:${THE_CLASSPATH}" ANTLR/generated/*.java src/task/*.java src/object/*.java src/driver/*.java src/client/*.java
 
-java -cp ${THE_CLASSPATH} client.Main
+java -cp ${THE_CLASSPATH} client.Main $1
 
 sleep 2
